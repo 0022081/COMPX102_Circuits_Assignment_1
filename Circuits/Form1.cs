9eae1410b7cb5cb85dec966dd7d2eb18f9126e8a
Benjamin <160984517+0022081@users.noreply.github.com>
2026-09-06 22:12:34 +1200

    Introduce AND, OR, NOT and PAD gates. Refactored Pin class to work with all gate classes.

--- Circuits/Form1.cs
+++ Circuits/Form1.cs
@@ -172,6 +172,36 @@ namespace Circuits
         }
 
         /// <summary>
+        /// This will create a new Or gate.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void toolStripButtonNot_Click(object sender, EventArgs e)
+        {
+            newGate = new NotGate(0, 0);
+        }
+
+        /// <summary>
+        /// This will create a new Or gate.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void toolStripButtonOr_Click(object sender, EventArgs e)
+        {
+            newGate = new OrGate(0, 0);
+        }
+
+        /// <summary>
+        /// This will creat a new Pad gate.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            newGate = new PadGate(0, 0);
+        }
+
+        /// <summary>
         /// Redraws all the graphics for the current circuit.
         /// </summary>
         /// <param name="sender"></param>
@@ -202,6 +232,10 @@ namespace Circuits
                 newGate.Draw(e.Graphics);
             }
         }
+
+       
+
+
 
         /// <summary>
         /// Handles events while the mouse button is pressed down.
